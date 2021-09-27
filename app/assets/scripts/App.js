3fab24166b59108e0a0c3efc97f611e8fcbd6966
Ben <benjaminbrockdutton@gmail.com>
2021-09-27 17:44:38 -0400

    finished reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,12 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu.js'
+import RevealOnScroll from './modules/RevealOnScroll.js'
 
 if (module.hot) {
     module.hot.accept();
 }
 
 const mobileMenu = new MobileMenu;
+new RevealOnScroll(document.querySelectorAll(".feature-item"));
+new RevealOnScroll(document.querySelectorAll(".testimonial"));
 
