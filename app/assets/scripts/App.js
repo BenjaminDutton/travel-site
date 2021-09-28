1766047a6f74019f239b104c61551c27d5584fc9
Ben <benjaminbrockdutton@gmail.com>
2021-09-28 14:14:48 -0400

    completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from './modules/RevealOnScroll.js'
+import StickyHeader from './modules/StickyHeader.js'
 
 if (module.hot) {
     module.hot.accept();
@@ -9,4 +10,5 @@ if (module.hot) {
 const mobileMenu = new MobileMenu;
 new RevealOnScroll(document.querySelectorAll(".feature-item"));
 new RevealOnScroll(document.querySelectorAll(".testimonial"));
+const stickyHeader = new StickyHeader();
 
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,6 +2,7 @@ class RevealOnScroll {
     constructor(el) {
         this.itemsToReveal = el;
         this.hideInitially();
+        this.browserHeight = window.innerHeight;
         this.events();
     }
 
@@ -9,12 +10,12 @@ class RevealOnScroll {
         window.addEventListener('scroll', () => {
             this.itemsToReveal.forEach(el => {
                 this.calculateIfScrolledTo(el)
-            })
+            });
         })
     }
 
     calculateIfScrolledTo(el) {
-        let scrollPercent = (el.getBoundingClientRect().top / window.innerHeight) * 100;
+        let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100;
         if (scrollPercent < 90) {
             el.classList.add('reveal-item--is-visible');
         }
